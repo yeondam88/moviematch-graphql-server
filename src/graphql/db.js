4f7cb3599b25131eaeac132c26d16974e6020b29
Yeondam Park <giyen9088@gmail.com>
2018-09-11 23:07:10 -0700

    adding endpoint to get themoviedb data

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -8,59 +8,161 @@ const BASE_URL = "https://api.themoviedb.org/3";
  * @param {string} time_window possible options - week or day
  * @param {string} media_type possible options - movie, tv, person, all
  */
-export const GET_TREND_MOVIES = async (
-  time_window = "week",
-  media_type = "movie"
+export const getTrendMovies = async (
+  media_type = "movie",
+  time_window = "week"
 ) => {
   try {
     const {
-      data: {
-        results: { movies }
-      }
-    } = await axios.get(
-      `${BASE_URL}/trending/${media_type}/${time_window}?api_key=${
-        process.env.THEMOVIEDB_API_KEY
-      }&language=en-US&page=1&region=us`
-    );
+      data: { results }
+    } = await axios.get(`${BASE_URL}/trending/${media_type}/${time_window}`, {
+      params: { api_key: "86fce8bfeb204a7e8c71d14290ae5016" }
+    });
+    return results;
   } catch (err) {
     Promise.reject(err);
   }
-  return movies;
 };
 
 /**
  * GET NOW PLAYING MOVIES
  * Get now playing movies in USA
  */
-export const GET_NOWPLAYING_MOVIES = async () => {
+export const getNowPlaying = async () => {
   try {
     const {
       data: { results }
-    } = await axios.get(
-      `${BASE_URL}/movie/now_playing?api_key=${
-        process.env.THEMOVIEDB_API_KEY
-      }&language=en-US&page=1&region=us`
-    );
+    } = await axios.get(`https://api.themoviedb.org/3/movie/now_playing`, {
+      params: {
+        api_key: "86fce8bfeb204a7e8c71d14290ae5016",
+        language: "en-US",
+        region: "us",
+        page: 1
+      }
+    });
+    return results;
   } catch (err) {
     Promise.reject(err);
   }
-  return results;
 };
 
-export const GET_UPCOMING_MOVIES = async () => {
+/**
+ * GET Upcoming movies
+ */
+export const getupcomingMovies = async () => {
   try {
     const {
-      data: {
-        results: { movies }
+      data: { results }
+    } = await axios.get(`https://api.themoviedb.org/3/movie/upcoming`, {
+      params: {
+        api_key: "86fce8bfeb204a7e8c71d14290ae5016",
+        language: "en-US",
+        region: "us",
+        page: 1
       }
-    } = await axios.get(
-      `${BASE_URL}/movie/upcoming?api_key=${
-        process.env.THEMOVIEDB_API_KEY
-      }&language=en-US&page=1&region=us`
-    );
+    });
+    return results;
   } catch (err) {
     Promise.reject(err);
   }
+};
 
-  return movies;
+/**
+ * GET MOVIE RECOMMANDATION base on currently viewing movie
+ * @param {number} id
+ */
+export const getMovieRecommendation = async id => {
+  try {
+    const {
+      data: { results }
+    } = await axios.get(`${BASE_URL}/movie/${id}/recommendations`, {
+      params: {
+        api_key: "86fce8bfeb204a7e8c71d14290ae5016",
+        language: "en-US",
+        region: "us",
+        page: 1
+      }
+    });
+    return results;
+  } catch (err) {
+    Promise.reject(err);
+  }
+};
+
+/**
+ * GET POPULAR MOVIES
+ */
+export const getPopularMovies = async () => {
+  try {
+    const {
+      data: { results }
+    } = await axios.get(`${BASE_URL}/movie/popular`, {
+      params: {
+        api_key: "86fce8bfeb204a7e8c71d14290ae5016",
+        language: "en-US",
+        region: "us"
+      }
+    });
+    return results;
+  } catch (err) {
+    Promise.reject(err);
+  }
+};
+
+/**
+ * GET POPULAR MOVIES
+ */
+export const getTopRatedMovies = async () => {
+  try {
+    const {
+      data: { results }
+    } = await axios.get(`${BASE_URL}/movie/top_rated`, {
+      params: {
+        api_key: "86fce8bfeb204a7e8c71d14290ae5016",
+        language: "en-US",
+        region: "us"
+      }
+    });
+    return results;
+  } catch (err) {
+    Promise.reject(err);
+  }
+};
+
+/**
+ * SEARCH MOVIES by ID
+ * @param {string} query
+ */
+export const searchMovies = async query => {
+  try {
+    const {
+      data: { results }
+    } = await axios.get(`${BASE_URL}/search/movie/`, {
+      params: {
+        api_key: "86fce8bfeb204a7e8c71d14290ae5016",
+        language: "en-US",
+        query,
+        region: "us",
+        page: 1
+      }
+    });
+    return results;
+  } catch (err) {
+    Promise.reject(err);
+  }
+};
+
+/**
+ * GET MOVIE DETAIL by ID
+ * @param {number} id
+ */
+export const getMovieDetail = async id => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/movie/${id}`, {
+      params: { api_key: "86fce8bfeb204a7e8c71d14290ae5016", language: "en-US" }
+    });
+    return data;
+  } catch (err) {
+    Promise.reject(err);
+  }
 };
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,26 @@
+import {
+  getTrendMovies,
+  getNowPlaying,
+  getupcomingMovies,
+  getMovieRecommendation,
+  getMovieDetail,
+  getPopularMovies,
+  getTopRatedMovies,
+  searchMovies
+} from "./db";
+
 const resolvers = {
   Query: {
-    movies: (_, {})
+    trendMovies: (_, { media_type, time_window }) =>
+      getTrendMovies(media_type, time_window),
+    nowPlayingMovies: () => getNowPlaying(),
+    upcomingMovies: () => getupcomingMovies(),
+    movieRecommendations: (_, { id }) => getMovieRecommendation(id),
+    movieDetail: (_, { id }) => getMovieDetail(id),
+    popularMovies: () => getPopularMovies(),
+    topRatedMovies: () => getTopRatedMovies(),
+    search: (_, { query }) => searchMovies(query)
   }
 };
+
+export default resolvers;
