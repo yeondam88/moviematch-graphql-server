0a162f8bcb22385941bbc368afea19bc5e20f50b
Yeondam Park <giyen9088@gmail.com>
2018-09-11 23:17:51 -0700

    config options

--- src/index.js
+++ src/index.js
@@ -6,4 +6,15 @@ const server = new GraphQLServer({
   resolvers
 });
 
-server.start(() => console.log("Graphql Server Running"));
+const options = {
+  port: 8000,
+  endpoint: "/graphql",
+  subscriptions: "/subscriptions",
+  playground: "/playground"
+};
+
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`
+  )
+);
